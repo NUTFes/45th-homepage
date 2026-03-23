3df2c50e19fe3f85ec8b39d20903f72c771f6999
oniwawa <173151966+oniwawa@users.noreply.github.com>
2026-03-23 14:04:00 +0000

    style: auto format by oxfmt

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
 export default function Page() {
   return (
-  <div className="fixed top-0 w-full bg-white px-3 py-5 gap-5 flex items-center z-10">
-    <Image src="/icon/45th-logo-top.svg" alt="Logo" width={48} height={48}/>
-    <div className="text-title text-base-dark font-kaisotai">45th NUTFES</div>
-  </div>
-
+    <div className="fixed top-0 w-full bg-white px-3 py-5 gap-5 flex items-center z-10">
+      <Image src="/icon/45th-logo-top.svg" alt="Logo" width={48} height={48} />
+      <div className="text-title text-base-dark font-kaisotai">45th NUTFES</div>
+    </div>
   );
-} 
+}
