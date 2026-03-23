0245f6624bfdd44b63f3be6b3e9e26d6078728b3
oniwawa <s243336@stn.nagaokaut.ac.jp>
2026-03-23 22:33:51 +0900

    feat:ヘッダーにロゴ画像を挿入

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,3 +1,11 @@
+import Image from "next/image";
 export default function Page() {
-  return <div>Page</div>;
-} /*仮置きのものなので消してもらって大丈夫です*/
+  return (
+  <div className="fixed top-0 w-full bg-white px-3 py-5 gap-5 flex">
+    <Image src="/icon/45th-logo-top.svg" alt="Logo" width={48} height={48}/>
+    <></>
+
+  </div>
+
+  );
+} 
