7b452420f7cacde74b62aa3f3c0a020ab929653b
kiyo-jin <25.m.jinnguuji.nutfes@gmail.com>
2026-03-24 01:47:35 +0900

    refactor:サイズ変更

--- src/components/layout/Bottom.tsx
+++ src/components/layout/Bottom.tsx
@@ -1,11 +1,11 @@
 import { House, Clock, MapPin, CalendarDays, Menu} from 'lucide-react';
 
 const navItems = [
-    { name: 'ホーム' , icon: House,  size: 36},
-    { name: 'スケジュール' , icon: Clock,  size: 36},
-    { name: 'マップ' , icon: MapPin,  size: 36},
-    { name: '企画' , icon: CalendarDays,  size: 36},
-    { name: 'メニュー' , icon: Menu,  size: 36},
+    { name: 'ホーム' , icon: House},
+    { name: 'スケジュール' , icon: Clock},
+    { name: 'マップ' , icon: MapPin},
+    { name: '企画' , icon: CalendarDays},
+    { name: 'メニュー' , icon: Menu},
 ];
 
 export default function BottomNavigation(){
@@ -13,7 +13,7 @@ export default function BottomNavigation(){
         <nav className='flex justify-between bg-base-dark fixed bottom-0 right-0 left-0 px-s py-ss md:hidden'>
             {navItems.map((item) => (
                 <div key = {item.name} className = 'flex flex-col items-center flex-1 h-[57px] justify-center gap-1'>
-                    <item.icon  className = 'text-secondary shrink-0' size={item.size}/>
+                    <item.icon  className = 'text-secondary shrink-0' size={36}/>
                     <span className='text-secondary text-text-small text-small--font-weight whitespace-nowrap'>{item.name}</span>
                 </div>
             ))}
