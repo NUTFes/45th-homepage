a812697442bc044f59a29a9ed8caa7a0edbd0e13
oniwawa <173151966+oniwawa@users.noreply.github.com>
2026-03-23 16:09:22 +0000

    style: auto format by oxfmt

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,12 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Page() {
   return (
-  <header className="sticky top-0 w-full bg-white px-xs py-m gap-m flex items-center">
-    <Link href="/">
+    <header className="sticky top-0 w-full bg-white px-xs py-m gap-m flex items-center">
+      <Link href="/">
         <Image src="/icon/45th-logo-top.svg" alt="45thNUTFES ロゴ" width={48} height={48} />
-    </Link>
-    <div className="text-title text-base-dark font-kaisotai">45th NUTFES</div>
-  </header>
-
+      </Link>
+      <div className="text-title text-base-dark font-kaisotai">45th NUTFES</div>
+    </header>
   );
 }
