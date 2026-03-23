b1981d6039bc96e69c049b7a1af6881b3a922c14
oniwawa <s243336@stn.nagaokaut.ac.jp>
2026-03-24 01:02:32 +0900

    feat:記述を変更

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 export default function Page() {
   return (
-    <div className="fixed top-0 w-full bg-white px-3 py-5 gap-5 flex items-center z-10">
-      <Image src="/icon/45th-logo-top.svg" alt="Logo" width={48} height={48} />
-      <div className="text-title text-base-dark font-kaisotai">45th NUTFES</div>
-    </div>
+  <header className="sticky top-0 w-full bg-white px-xs py-m gap-5 flex items-center">
+    <Link href="/">
+        <Image src="/icon/45th-logo-top.svg" alt="45thNUTFES ロゴ" width={48} height={48} />
+    </Link>
+    <div className="text-title text-base-dark font-kaisotai">45th NUTFES</div>
+  </header>
+
   );
 }
