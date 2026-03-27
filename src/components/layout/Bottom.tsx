2af41fd7f311e3b8867fe4ff139f62d63baa3d1e
kiyo-jin <25.m.jinnguuji.nutfes@gmail.com>
2026-03-27 11:58:11 +0900

    refactor: nav > ul > li > Link のセマンティック構造に変更

--- src/components/layout/Bottom.tsx
+++ src/components/layout/Bottom.tsx
@@ -1,22 +1,32 @@
-import { House, Clock, MapPin, CalendarDays, Menu} from 'lucide-react';
+import { House, Clock, MapPin, CalendarDays, Menu } from "lucide-react";
+import Link from "next/link";
 
 const navItems = [
-    { name: 'ホーム' , icon: House},
-    { name: 'スケジュール' , icon: Clock},
-    { name: 'マップ' , icon: MapPin},
-    { name: '企画' , icon: CalendarDays},
-    { name: 'メニュー' , icon: Menu},
+  { name: "ホーム", icon: House, href: "/" },
+  { name: "スケジュール", icon: Clock, href: "/" },
+  { name: "マップ", icon: MapPin, href: "/" },
+  { name: "企画", icon: CalendarDays, href: "/" },
+  { name: "メニュー", icon: Menu, href: "/" },
 ];
 
-export default function BottomNavigation(){
-    return (
-        <nav className='flex justify-between bg-base-dark sticky bottom-0 right-0 left-0 px-s py-ss md:hidden'>
-            {navItems.map((item) => (
-                <div key = {item.name} className = 'flex flex-col items-center flex-1 h-[57px] justify-center gap-1'>
-                    <item.icon  className = 'text-secondary shrink-0' size={36}/>
-                    <span className='text-secondary text-text-small text-small--font-weight whitespace-nowrap'>{item.name}</span>
-                </div>
-            ))}
-        </nav>
-    )
+export default function BottomNavigation() {
+  return (
+    <nav className="flex bg-base-dark sticky bottom-0 right-0 left-0 px-s py-ss md:hidden">
+      <ul className="flex justify-between w-full list-none">
+        {navItems.map((item) => (
+          <li key={item.name} className="flex-1">
+            <Link
+              href={item.href}
+              className="flex flex-col items-center h-[57px] justify-center gap-1"
+            >
+              <item.icon className="text-secondary shrink-0" size={36} />
+              <span className="text-secondary text-text-small text-small--font-weight whitespace-nowrap">
+                {item.name}
+              </span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
