2973ed158fb0cde5c6495fba9e18a053f9e2ef46
kiyo-jin <25.m.jinnguuji.nutfes@gmail.com>
2026-03-27 11:06:16 +0900

    fix: fixedからstickyに修正

--- src/components/layout/Bottom.tsx
+++ src/components/layout/Bottom.tsx
@@ -10,7 +10,7 @@ const navItems = [
 
 export default function BottomNavigation(){
     return (
-        <nav className='flex justify-between bg-base-dark fixed bottom-0 right-0 left-0 px-s py-ss md:hidden'>
+        <nav className='flex justify-between bg-base-dark sticky bottom-0 right-0 left-0 px-s py-ss md:hidden'>
             {navItems.map((item) => (
                 <div key = {item.name} className = 'flex flex-col items-center flex-1 h-[57px] justify-center gap-1'>
                     <item.icon  className = 'text-secondary shrink-0' size={36}/>
