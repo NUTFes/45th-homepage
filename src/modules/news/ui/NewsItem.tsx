0941a30c47279f5ede570463c9de7cd61c882da7
kiyo-jin <25.m.jinnguuji.nutfes@gmail.com>
2026-03-29 17:52:13 +0900

    feat: NewsItemコンポーネントのついか

--- src/modules/news/ui/NewsItem.tsx
+++ src/modules/news/ui/NewsItem.tsx
@@ -1,3 +1,16 @@
-export default function NewsItem(){
-    <div></div>
+type Props = {
+  date: string;
+  title: string;
+  content: string;
+};
+
+export default function NewsItem({ date, title, content }: Props) {
+  const displayDate = date.replaceAll("-", ".");
+  return (
+    <article className="flex flex-col gap-ss border-b border-font-main px-ss pb-ss text-text text-font-main">
+      <time dateTime={date}>{displayDate}</time>
+      <h2 className="text-button -ml-[0.5em]">{`［ ${title} ］`}</h2>
+      <p className="text-justify whitespace-pre-wrap">{content}</p>
+    </article>
+  );
 }
