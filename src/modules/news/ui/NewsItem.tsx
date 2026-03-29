a8444742ddae284281a6b1d006cb696a04102368
kiyo-jin <25.m.jinnguuji.nutfes@gmail.com>
2026-03-29 17:54:24 +0900

    fix: ［と空白の問題を修正

--- src/modules/news/ui/NewsItem.tsx
+++ src/modules/news/ui/NewsItem.tsx
@@ -9,7 +9,9 @@ export default function NewsItem({ date, title, content }: Props) {
   return (
     <article className="flex flex-col gap-ss border-b border-font-main px-ss pb-ss text-text text-font-main">
       <time dateTime={date}>{displayDate}</time>
-      <h2 className="text-button -ml-[0.5em]">{`［ ${title} ］`}</h2>
+      <h2 className="text-button -ml-[0.5em] before:content-['［_'] after:content-['_］']">
+        {title}
+      </h2>
       <p className="text-justify whitespace-pre-wrap">{content}</p>
     </article>
   );
