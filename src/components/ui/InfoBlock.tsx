0f9597168e578c5af2e7417a52c621d1931a0068
oniwawa <s243336@stn.nagaokaut.ac.jp>
2026-03-29 23:02:03 +0900

    feat:InfoBlockを作成

---
+++ src/components/ui/InfoBlock.tsx
@@ -0,0 +1,28 @@
+import InfoFrame from "@/components/ui/InfoFrame";
+import * as Icons from "lucide-react";
+
+//Icon="~"で使用したいアイコンの名前を指定　例：Icon="Camera"
+//title="~"でタイトルの文字列を入力
+//childrenに本文を入力（ReactNodeを使用可能）
+export default function InfoBlock(props: { Icon: string; title:string ;children: React.ReactNode }) {
+    const { Icon , title,children} = props;
+    const LucideIcon = (Icons as any)[Icon];
+    return(
+        <InfoFrame>
+            <div className="gap-y-ss">
+                <div className="flex justify-content items-center px-ss pb-1 gap-2.5 border-b border-white">
+                    {LucideIcon ? (
+                    <LucideIcon size={28} />
+                    ) : (
+                    <Icons.HelpCircle size={28} />//見つからない時に代わりに表示するアイコン
+                    )}
+                    <div className="text-title-small white">{title}</div>
+                    
+                </div>
+                <div className="pl-3 text">
+                    {children}
+                </div>
+            </div>
+        </InfoFrame>
+    );
+}
--- src/components/ui/InfoFrame.tsx
+++ src/components/ui/InfoFrame.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-export default function Frame(props: { children: React.ReactNode }) {
+export default function InfoFrame(props: { children: React.ReactNode }) {
   const { children } = props;
 
   return (
     <div className="px-ll">
     <div className="bg-main">
-        <div className="bg-base-dark border-main border-2 rounded-3xl text-white flex px-m py-ll">
+        <div className="bg-base-dark border-main border-2 rounded-3xl text-white px-m py-ll  gap-y-8">
             {children}
         </div>
     </div>
