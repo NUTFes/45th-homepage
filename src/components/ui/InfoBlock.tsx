840c18cf8bac479685abda0bf9e2f1f472ec06fb
oniwawa <s243336@stn.nagaokaut.ac.jp>
2026-04-01 17:56:05 +0900

    fix:InfoBlockを辞書参考の仕様に変更

--- src/components/ui/InfoBlock.tsx
+++ src/components/ui/InfoBlock.tsx
@@ -1,4 +1,4 @@
-import * as Icons from "lucide-react";
+import {Clock,TriangleAlert,Tickets,Users,DoorOpen,CameraOff,CandyOff,CigaretteOff,Beer,BusFront,HelpCircle} from "lucide-react";
 
 //Icon="~"で使用したいアイコンの名前を指定　例：Icon="Camera"
 //title="~"でタイトルの文字列を入力
@@ -9,14 +9,27 @@ export default function InfoBlock(props: {
   children: React.ReactNode;
 }) {
   const { Icon, title, children } = props;
-  const LucideIcon = (Icons as any)[Icon];
+const iconMap = {
+Clock,
+TriangleAlert,
+Tickets,
+Users,
+DoorOpen,
+CameraOff,
+CandyOff,
+CigaretteOff,
+Beer,
+BusFront,
+HelpCircle
+} as const;
+ const LucideIcon = iconMap[Icon as keyof typeof iconMap] ?? HelpCircle;
   return (
     <div className="flex flex-col gap-ss">
       <div className="flex w-fit items-center gap-2.5 border-b border-white px-ss pb-1">
         {LucideIcon ? (
           <LucideIcon size={28} />
         ) : (
-          <Icons.HelpCircle size={28} /> //見つからない時に代わりに表示するアイコン
+          <HelpCircle size={28} /> //見つからない時に代わりに表示するアイコン
         )}
         <div className="text-title-small text-white">{title}</div>
       </div>
