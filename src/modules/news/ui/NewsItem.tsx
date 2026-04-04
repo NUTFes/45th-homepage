40f8a1cc92c06cb8afdaf81140ec4eb7350d50fa
kiyo-jin <25.m.jinnguuji.nutfes@gmail.com>
2026-04-04 18:29:02 +0900

    fix: NewsItemの型名をNewsItemPropsに変更

--- src/modules/news/ui/NewsItem.tsx
+++ src/modules/news/ui/NewsItem.tsx
@@ -1,6 +1,6 @@
-import type { NewsItem } from "../types";
+import type { NewsItemProps } from "../types";
 
-export default function NewsItem({ date, title, content }: NewsItem) {
+export default function NewsItem({ date, title, content }: NewsItemProps) {
   const displayDate = date.replaceAll("-", ".");
   return (
     <li className="flex flex-col gap-ss border-b border-font-main px-ss pb-ss text-text text-font-main">
