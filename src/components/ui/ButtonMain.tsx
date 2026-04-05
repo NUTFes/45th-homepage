9be9c1b49ca1fe2de0973d35424ae4325bd153a2
oniwawa <s243336@stn.nagaokaut.ac.jp>
2026-04-05 23:22:21 +0900

    feat:ボタンを完成

--- src/components/ui/ButtonMain.tsx
+++ src/components/ui/ButtonMain.tsx
@@ -6,14 +6,15 @@ import { Button } from "../aria/Button";
 export default function ButtonMain(props: { href: string ,title: string }) {
     const { href, title } = props;
     return (
-<Button className=" w-fit h-fit button-gradient rounded-full border-2 border-main">
-<Link
-    href={href}
-  className="font-sans text-white text-button px-l py-s"
->
-  {title}
-</Link>
-</Button>
+    <div className="pb-ss">
+    {/* ↑影のぶんのパディング */}
+    <Link
+        href={href}
+        className="button-gradient py-s px-l rounded-full border-2 border-main text-white text-button hover:text-main transition-colors duration-200 shadow-[0px_6px_8px_rgba(60,224,232,1.0)]"
+        >
+    {title}
+    </Link>
+    </div>
     );
 }
         
