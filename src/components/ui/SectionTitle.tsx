2ab6f910b1a081d58129ed70dba11469c1562475
kiyo-jin <209897993+kiyo-jin@users.noreply.github.com>
2026-04-05 15:59:23 +0000

    style: auto format by oxfmt

--- src/components/ui/SectionTitle.tsx
+++ src/components/ui/SectionTitle.tsx
@@ -5,9 +5,7 @@ type SectionTitleProps = {
 export default function SectionTitle({ title }: SectionTitleProps) {
   return (
     <div className="inline-block border-b-[1.6px] border-button-line px-l">
-      <div
-        className="font-kaisotai text-title text-font-main text-shadow-[1px_2px_2px_var(--color-base-dark)]"
-      >
+      <div className="font-kaisotai text-title text-font-main text-shadow-[1px_2px_2px_var(--color-base-dark)]">
         {title}
       </div>
     </div>
