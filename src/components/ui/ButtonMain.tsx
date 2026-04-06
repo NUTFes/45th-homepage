3ee8b8e53d0d6b3b6c630b407be04434d463e8c3
oniwawa <s243336@stn.nagaokaut.ac.jp>
2026-04-06 22:51:19 +0900

    feat:ホバーアニメーションを追加

--- src/components/ui/ButtonMain.tsx
+++ src/components/ui/ButtonMain.tsx
@@ -4,10 +4,16 @@ export default function ButtonMain(props: { href: string; title: string }) {
   const { href, title } = props;
   return (
     <div className="flex w-full justify-center pb-ss">
-      {/* ↑影のぶんのパディング */}
       <Link
         href={href}
-        className="button-gradient rounded-full border-2 border-main px-l py-s text-button text-white shadow-[0px_6px_8px_rgba(60,224,232,1.0)] transition-colors duration-200 hover:text-main"
+        className="
+          button-gradient rounded-full border-2 border-main px-l py-s
+          text-button text-white
+          shadow-[0px_6px_8px_rgba(60,224,232,0.6)]
+          transition-all duration-200
+          hover:-translate-y-1
+          hover:shadow-[0px_6px_8px_rgba(60,224,232,1.0)]
+        "
       >
         {title}
       </Link>
