c06fcdea154d02492a30ba8f5082b5571df8d1a4
kiyo-jin <25.m.jinnguuji.nutfes@gmail.com>
2026-04-11 15:17:23 +0900

    fix: 説明文にpx-12を設けた

--- src/modules/top/ui/LogoInfo.tsx
+++ src/modules/top/ui/LogoInfo.tsx
@@ -4,7 +4,7 @@ export default function LogoInfo(){
     return(
         <div className="bg-linear-to-b from-[#FFAFA] from-7% via-[#9399C7] to-base flex flex-col  items-center justify-center w-full pt-4l pb-3l gap-ll">
             <Image src="/image/45th-logo.svg" alt="45th技大祭ロゴ" width={140} height={140} />
-            <p className="text-center text-text text-font-main">
+            <p className="text-center text-text text-font-main px-12">
                 これは説明文です。これは説明文です。これは説明文です。
                 これは説明文です。これは説明文です。
                 これは説明文です。これは説明文です。これは説明文です。
