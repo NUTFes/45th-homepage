3bc0bfa0a7b796e6e49575e1d570fa8fe56179fa
TkymHrt <23.h.takayama.nutfes@gmail.com>
2026-04-12 17:38:53 +0900

    feat: update getNews function and add news pagination logic

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       startServerCommand: "pnpm run start",
-      url: ["http://localhost:3000/"],
+      url: ["http://localhost:3000/", "http://localhost:3000/news"],
       numberOfRuns: 3,
     },
     upload: {
