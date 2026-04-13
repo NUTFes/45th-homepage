8f421e6257e56917def5572dd52e3355c1a7cb76
kiyo-jin <209897993+kiyo-jin@users.noreply.github.com>
2026-04-13 02:38:35 +0000

    style: auto format by oxfmt

--- src/modules/top/ui/LogoInfo.tsx
+++ src/modules/top/ui/LogoInfo.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
 
-export default function LogoInfo(){
-    return(
-        <section className="bg-linear-to-b from-[#FFFAFA] from-7% via-[#9399C7] to-base flex flex-col  items-center justify-center w-full pt-4l pb-3l gap-ll">
-            <Image src="/image/45th-logo.svg" alt="45th技大祭ロゴ" width={140} height={140} />
-            <p className="text-center text-text text-font-main px-12">
-                これは説明文です。これは説明文です。これは説明文です。
-                これは説明文です。これは説明文です。
-                これは説明文です。これは説明文です。これは説明文です。
-            </p>
-        </section>
-    );
+export default function LogoInfo() {
+  return (
+    <section className="flex w-full flex-col items-center justify-center gap-ll bg-linear-to-b from-[#FFFAFA] from-7% via-[#9399C7] to-base pt-4l pb-3l">
+      <Image src="/image/45th-logo.svg" alt="45th技大祭ロゴ" width={140} height={140} />
+      <p className="px-12 text-center text-text text-font-main">
+        これは説明文です。これは説明文です。これは説明文です。 これは説明文です。これは説明文です。
+        これは説明文です。これは説明文です。これは説明文です。
+      </p>
+    </section>
+  );
 }
