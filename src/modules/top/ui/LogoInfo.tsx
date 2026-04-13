7617888927f192cfdafda26a00698ae8e444fc7b
kiyo-jin <25.m.jinnguuji.nutfes@gmail.com>
2026-04-13 11:04:21 +0900

    fix: カラーコードの修正

--- src/modules/top/ui/LogoInfo.tsx
+++ src/modules/top/ui/LogoInfo.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function LogoInfo(){
     return(
-        <div className="bg-linear-to-b from-[#FFAFA] from-7% via-[#9399C7] to-base flex flex-col  items-center justify-center w-full pt-4l pb-3l gap-ll">
+        <section className="bg-linear-to-b from-[#FFFAFA] from-7% via-[#9399C7] to-base flex flex-col  items-center justify-center w-full pt-4l pb-3l gap-ll">
             <Image src="/image/45th-logo.svg" alt="45th技大祭ロゴ" width={140} height={140} />
             <p className="text-center text-text text-font-main px-12">
                 これは説明文です。これは説明文です。これは説明文です。
                 これは説明文です。これは説明文です。
                 これは説明文です。これは説明文です。これは説明文です。
             </p>
-        </div>
+        </section>
     );
 }
