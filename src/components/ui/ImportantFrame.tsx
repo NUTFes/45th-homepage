8fe443ef608340f11d2f21ce8202c3b4e20424de
ponyo1114 <kakesou1114@gmail.com>
2026-04-16 04:49:06 +0900

    fix:gapの変更

--- src/components/ui/ImportantFrame.tsx
+++ src/components/ui/ImportantFrame.tsx
@@ -8,7 +8,7 @@ type ImportantFrameProps = {
 export default function ImportantFrame({ title, children }: ImportantFrameProps) {
 	return (
 		<div className="w-full border-y-[1.4px] border-main bg-base-dark px-3l py-l">
-			<div className="flex w-full flex-col gap-[2px]">
+			<div className="flex w-full flex-col gap-0.5">
 				<h3 className="font-sans text-title-small leading-7.25 text-[#ff5bef]">{title}</h3>
 				<p className="font-sans text-text-large text-white">{children}</p>
 			</div>
