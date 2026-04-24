a060783d576fa8bf46d4f2a55811e7d1f77ff2b1
kiyo-jin <25.m.jinnguuji.nutfes@gmail.com>
2026-04-24 11:39:05 +0900

    fix: 画像サイズ、テキストスタイル、aria-labelの修正

--- src/modules/top/ui/PcLogoinfo.tsx
+++ src/modules/top/ui/PcLogoinfo.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function PcLogoInfo() {
   return (
-    <section className="flex w-full  items-center justify-center gap-pm bg-linear-to-b from-[#FFFAFA] from-[6.73%] via-[#9399C7] via-[50.48%] to-base py-ll px-pll">
-      <Image src="/image/45th-LogoBlue.svg" alt="45th技大祭ロゴ" width={256} height={256} />
-      <p className="px-12 text-center text-text text-font-main">
+    <section aria-label="技大祭について" className="flex w-full items-center justify-center gap-pm bg-linear-to-b from-[#FFFAFA] from-[8.17%] via-[#9399C7] via-[54.81%] to-base px-pll py-ll">
+      <Image src="/image/45th-LogoBlue.svg" alt="45th技大祭ロゴ" width={260} height={260} />
+      <p className="flex-1 text-center text-Ptext-large font-bold text-font-main">
         これは説明文です。これは説明文です。これは説明文です。 これは説明文です。これは説明文です。
         これは説明文です。これは説明文です。これは説明文です。
       </p>
