b53a521d516694fddb6b3c9e99f0d6a623440c68
oniwawa <s243336@stn.nagaokaut.ac.jp>
2026-04-29 19:50:54 +0900

    [fix]スマホ版のpxを修正

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Page() {
   return (
-    <header className="sticky top-0 flex w-full items-center justify-between bg-white px-5l py-m">
+    <header className="sticky top-0 flex w-full items-center justify-between bg-white px-m lg:px-m py-m">
       <div className="flex gap-xs items-center">
       <Link href="/">
         <Image src="/icon/45th-logo-top.svg" alt="45thNUTFES ロゴ" width={48} height={48} />
