e9620a10c14d5f5d28a653e656b97f326f36c9f6
TkymHrt <23.h.takayama.nutfes@gmail.com>
2026-04-29 17:45:58 +0900

    style: update component styles for responsiveness and consistency

--- src/components/ui/ButtonMain.tsx
+++ src/components/ui/ButtonMain.tsx
@@ -11,7 +11,7 @@ export default function ButtonMain(props: ButtonMainProps) {
   return (
     <Link
       href={href}
-      className="button-gradient rounded-full border-2 border-main px-l py-s text-button text-white shadow-[0px_6px_8px_rgba(60,224,232,0.6)] transition-all duration-200 hover:-translate-y-1 hover:shadow-[0px_6px_8px_rgba(60,224,232,1.0)]"
+      className="button-gradient rounded-full border-2 border-main px-l py-s text-button text-white shadow-[0px_6px_8px_rgba(60,224,232,0.6)] transition-all duration-200 hover:-translate-y-1 hover:shadow-[0px_6px_8px_rgba(60,224,232,1.0)] md:text-Pbutton"
     >
       {title}
     </Link>
--- src/components/ui/NewsItem.tsx
+++ src/components/ui/NewsItem.tsx
@@ -7,9 +7,9 @@ type NewsItemProps = {
 
 export default function NewsItem({ date, dateTime, title, content }: NewsItemProps) {
   return (
-    <li className="flex flex-col gap-ss border-b border-font-main px-ss pb-ss text-text text-font-main">
+    <li className="flex flex-col gap-ss border-b border-font-main px-ss pb-ss text-text text-font-main md:text-Ptext">
       <time dateTime={dateTime}>{date}</time>
-      <div className="-ml-[0.5em] text-button before:content-['［_'] after:content-['_］']">
+      <div className="-ml-[0.5em] text-button before:content-['［_'] after:content-['_］'] md:text-Pbutton">
         {title}
       </div>
       <p className="text-justify whitespace-pre-wrap">{content}</p>
