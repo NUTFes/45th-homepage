f7f2646174ac0c787aec6c0586e1e98baf9724de
oniwawa <173151966+oniwawa@users.noreply.github.com>
2026-04-29 11:59:29 +0000

    style: auto format by oxfmt

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,27 +3,19 @@ import Image from "next/image";
 import Link from "next/link";
 export default function Page() {
   return (
-    <header className="sticky top-0 flex w-full items-center justify-between bg-white px-m lg:px-m py-m">
-      <div className="flex gap-xs items-center">
-      <Link href="/">
-        <Image src="/icon/45th-logo-top.svg" alt="45thNUTFES ロゴ" width={48} height={48} />
-      </Link>
-      <div className="font-kaisotai text-title text-base-dark">45th NUTFES</div>
+    <header className="sticky top-0 flex w-full items-center justify-between bg-white px-m py-m lg:px-m">
+      <div className="flex items-center gap-xs">
+        <Link href="/">
+          <Image src="/icon/45th-logo-top.svg" alt="45thNUTFES ロゴ" width={48} height={48} />
+        </Link>
+        <div className="font-kaisotai text-title text-base-dark">45th NUTFES</div>
       </div>
-      <div className="hidden lg:flex text-button text-base-dark gap-4l">
-        <div className="text-font-gray">
-          企画情報
-        </div>
-        <div className="text-font-gray">
-          スケジュール
-        </div>
-       <div className="text-font-gray">
-          マップ
-        </div>
-        <div className="text-font-gray">
-          利用案内
-        </div>
-        <LucideMenu className="text-base-dark" size={36}/>
+      <div className="hidden gap-4l text-button text-base-dark lg:flex">
+        <div className="text-font-gray">企画情報</div>
+        <div className="text-font-gray">スケジュール</div>
+        <div className="text-font-gray">マップ</div>
+        <div className="text-font-gray">利用案内</div>
+        <LucideMenu className="text-base-dark" size={36} />
       </div>
     </header>
   );
