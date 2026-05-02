196f3ec67b2edc6731d00e5bd4601fea984f3c6e
ponyo1114 <kakesou1114@gmail.com>
2026-05-03 07:29:56 +0900

    feat:重要お知らせフレームのファイルを追加

---
+++ src/components/ui/PcImportantFrame.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+type ImportantFrameProps = {
+  title: string;
+  children: ReactNode;
+};
+
+export default function ImportantFrame({ title, children }: ImportantFrameProps) {
+  return (
+    <div className="w-full border-y-[1.4px] border-main bg-base-dark py-l">
+      <div className="mx-auto flex w-full max-w-260 flex-col gap-0.5 px-3l">
+        <h3 className="font-sans text-title-small leading-7.25 text-accent md:text-Ptitle-small">
+          {title}
+        </h3>
+        <div className="font-sans text-text-large text-white md:text-Ptext-large">{children}</div>
+      </div>
+    </div>
+  );
+}
