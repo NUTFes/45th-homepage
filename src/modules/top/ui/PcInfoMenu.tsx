01af8d3a4b2b10ebd8ac9a3477bf12f32f61bdfc
ponyo1114 <kakesou1114@gmail.com>
2026-05-03 11:17:16 +0900

    feat:PC用の案内一覧ファイルを追加

---
+++ src/modules/top/ui/PcInfoMenu.tsx
@@ -0,0 +1,69 @@
+import {
+  UserStar,
+  Building2,
+  TriangleAlert,
+  MessageCircleQuestionMark,
+  BusFront,
+} from "lucide-react";
+import Link from "next/link";
+
+const InfoMenuItems = [
+  {
+    name: "代表者挨拶",
+    icon: UserStar,
+    href: "/",
+    disabled: true,
+  },
+  {
+    name: "注意事項",
+    icon: TriangleAlert,
+    href: "/",
+    disabled: true,
+  },
+  {
+    name: "案内所・ヘルプ",
+    icon: MessageCircleQuestionMark,
+    href: "/",
+    disabled: true,
+  },
+  {
+    name: "アクセス",
+    icon: BusFront,
+    href: "/",
+    disabled: true,
+  },
+  {
+    name: "協賛企業一覧",
+    icon: Building2,
+    href: "/",
+    disabled: true,
+  },
+];
+
+export default function InfoMenu() {
+  return (
+    <nav className="bg-base">
+      <ul className="flex w-full list-none flex-col gap-s">
+        {InfoMenuItems.map((item) => (
+          <li key={item.name} className="px-3l">
+            {item.disabled ? (
+              <span aria-disabled="true" className="flex items-center gap-[10px] text-[#8892b0]">
+                <div className="flex h-6 w-6 shrink-0 items-center justify-center">
+                  <item.icon className="text-disabled" size={24} />
+                </div>
+                <span className="text-disabled text-title-small">{item.name}</span>
+              </span>
+            ) : (
+              <Link href={item.href} className="flex items-center gap-[10px]">
+                <div className="flex h-6 w-6 shrink-0 items-center justify-center">
+                  <item.icon className="text-secondary" size={24} />
+                </div>
+                <span className="text-title-small text-font-main">{item.name}</span>
+              </Link>
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
