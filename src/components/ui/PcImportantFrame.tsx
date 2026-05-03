bb7311ae9cc8fb1e848da3489bbd090d4e1f061b
ponyo1114 <kakesou1114@gmail.com>
2026-05-03 12:23:48 +0900

    fix:重要お知らせフレームの行間変更

--- src/components/ui/PcImportantFrame.tsx
+++ src/components/ui/PcImportantFrame.tsx
@@ -12,7 +12,7 @@ export default function PcImportantFrame({
   return (
     <div className="w-full border-y-[1.4px] border-main bg-base-dark px-pl py-3l shadow-[0px_2px_6px_0px_var(--color-base)]">
       <div className="mx-auto flex w-full max-w-260 flex-col gap-0.5">
-        <h3 className="font-sans text-Ptitle-small leading-7.25 text-accent">
+        <h3 className="font-sans text-Ptitle-small leading-8 text-accent">
           {title}
         </h3>
         <div className="font-sans text-Ptext-large text-white">{children}</div>
