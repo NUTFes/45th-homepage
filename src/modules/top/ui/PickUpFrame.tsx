04bd7840d892ccbc518c603074d0590098bebcfd
TkymHrt <23.h.takayama.nutfes@gmail.com>
2026-05-03 13:56:28 +0900

    feat: アスペクト比率を固定

--- src/modules/top/ui/PickUpFrame.tsx
+++ src/modules/top/ui/PickUpFrame.tsx
@@ -8,7 +8,7 @@ export const PickUpFrame = ({ children }: PickUpFrameProps) => {
   return (
     <div className="relative w-full py-1.5">
       <div aria-hidden className="pointer-events-none absolute inset-x-0 top-0 h-[1.2px] bg-main" />
-      <div className="relative h-69.5 w-full overflow-hidden border-y-[1.4px] border-button-line bg-base-dark md:h-110">
+      <div className="relative w-full overflow-hidden border-y-[1.4px] border-button-line bg-base-dark">
         {children}
       </div>
       <div
