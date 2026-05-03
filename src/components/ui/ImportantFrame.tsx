546b0399910abe5e9ff77abddd43b0ca0961f123
ponyo1114 <kakesou1114@gmail.com>
2026-05-03 13:58:07 +0900

    feat:スマホ用の重要お知らせフレームにshadowを追加

--- src/components/ui/ImportantFrame.tsx
+++ src/components/ui/ImportantFrame.tsx
@@ -7,7 +7,7 @@ type ImportantFrameProps = {
 
 export default function ImportantFrame({ title, children }: ImportantFrameProps) {
   return (
-    <div className="w-full border-y-[1.4px] border-main bg-base-dark py-l">
+    <div className="w-full border-y-[1.4px] border-main bg-base-dark py-l shadow-[0px_2px_6px_0px_var(--color-base)]">
       <div className="mx-auto flex w-full max-w-260 flex-col gap-0.5 px-3l">
         <h3 className="font-sans text-title-small leading-7.25 text-accent md:text-Ptitle-small">
           {title}
