2953f49a0716b300b52b9bb186000e06c911a7e0
ponyo1114 <kakesou1114@gmail.com>
2026-05-07 15:20:07 +0900

    fix:CSSの調整

--- src/modules/top/ui/SponsorSection.tsx
+++ src/modules/top/ui/SponsorSection.tsx
@@ -22,20 +22,20 @@ export default function SponsorSection() {
         <div className="flex flex-col items-center gap-y-s md:gap-y-m">
           <div className="flex flex-col items-center">
             <div className="border-b border-white">
-              <div className="max-w-full break-words pb-1 text-center font-kaisotai text-title text-shadow-[1px_2px_2px_rgba(8,18,94,1.0)] md:text-Ptitle">
+              <div className="w-fit pb-1 text-center font-kaisotai text-title text-shadow-[1px_2px_2px_rgba(8,18,94,1.0)] md:text-Ptitle">
                 企業協賛大募集！
               </div>
             </div>
           </div>
 
-          <div className="break-words px-ss text-text md:px-0 md:text-center md:text-Ptext">
+          <div className="px-ss text-text md:text-center md:text-Ptext">
             技大祭実行委員会では、企業の皆さまからのご協賛を募集しております。
             <br />
             ご関心をお持ちの方は、以下の資料をご覧いただき、メールにてご連絡ください。
           </div>
 
           <div className="shadow-[1px_2px_2px_rgba(8,18,94,1.0)]">
-            <Button className="forced-colors:none flex h-fit w-[225px] justify-start gap-x-s rounded-sm bg-white px-m py-m text-base-dark hover:bg-main md:w-[300px] md:rounded-md md:px-m md:py-m">
+            <Button className="h-fit w-[225px] justify-start gap-x-s rounded-sm bg-white px-m py-m text-base-dark hover:bg-main md:w-[300px] md:rounded-md md:px-m md:py-m">
               <div className="shrink-0">
                 <LucideFile size={24} />
               </div>
@@ -47,20 +47,22 @@ export default function SponsorSection() {
         </div>
 
         <div className="flex flex-col items-center gap-y-s md:gap-y-m">
-          <div className="break-words px-ss text-text md:px-0 md:text-center md:text-Ptext">
+          <div className="px-ss text-text md:text-center md:text-Ptext">
             協賛のお問い合わせは、以下のメールアドレスからご連絡ください。
           </div>
 
           <div className="shadow-[1px_2px_2px_rgba(8,18,94,1.0)]">
             <Button
               onPress={handleCopy}
-              className="forced-colors:none flex h-fit w-[225px] justify-start gap-x-s rounded-sm bg-white px-m py-s text-base-dark hover:bg-main md:w-[300px] md:rounded-md md:px-m md:py-m"
+              className="h-fit w-[225px] justify-start gap-x-s rounded-sm bg-white px-m py-s text-base-dark hover:bg-main md:w-[300px] md:rounded-md md:px-m md:py-m"
             >
               <div className="shrink-0">
                 <Copy size={24} />
               </div>
-              <div className="flex-1 text-start text-text-large md:text-center md:text-Ptext-large md:whitespace-nowrap">
-                メールアドレスをコピー
+              <div className="flex-1 text-start text-text-large md:text-Ptext-large md:whitespace-nowrap">
+                <span className="whitespace-nowrap">メールアドレスを</span>
+                <br className="md:hidden" />
+                <span>コピー</span>
               </div>
             </Button>
           </div>
