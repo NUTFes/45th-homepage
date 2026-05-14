3228ffd4cdbb60b8bb4b56085efb168d7cdba1de
TkymHrt <23.h.takayama.nutfes@gmail.com>
2026-05-14 10:04:31 +0900

    feat: フォントのサブセット化

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,37 @@ const dirname = path.dirname(__filename);
 const nextConfig: NextConfig = {
   cacheComponents: true,
   output: "standalone",
+  async headers() {
+    return [
+      {
+        source: "/font/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/image/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/favicon/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       ...webpackConfig.resolve.extensionAlias,
