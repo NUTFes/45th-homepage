86a16f3d51d5c4e6852ba716304d36d4d6aed81f
kiyo-jin <25.m.jinnguuji.nutfes@gmail.com>
2026-05-24 22:07:20 +0900

    feat: ツールチップの実装

--- src/components/ui/EventFrame.tsx
+++ src/components/ui/EventFrame.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
-
+import { TooltipTrigger, Focusable } from "react-aria-components";
+import { Tooltip } from "@/components/aria/Tooltip";
 
 type EventFrameProps = {
   name: string;
@@ -9,25 +12,57 @@ type EventFrameProps = {
   imageAlt: string;
 };
 
-const MAX_LENGTH = 24; 
+const MAX_LENGTH = 24;
 const TEXTB_LENGTH = 14;
 
 export default function EventFrame(props: EventFrameProps) {
   const { name, href, imageUrl, imageAlt } = props;
 
-  const displayName = name.length > MAX_LENGTH ? name.slice(0, MAX_LENGTH) + "・・・" : name;
+  const isTruncated = name.length > MAX_LENGTH;
+
+  const displayName = isTruncated
+    ? name.slice(0, MAX_LENGTH-1) + "…"
+    : name;
+
   const nameClassName =
-    name.length <= TEXTB_LENGTH ? "text-textb" : "text-[14px] leading-[20px]";
+    name.length <= TEXTB_LENGTH
+      ? "text-textb"
+      : "text-[14px] leading-[20px]";
 
   return (
     <Link
       href={href}
-      className="flex flex-col  pb-s rounded-lg bg-secondary transition-all duration-200 hover:-translate-y-1 h-[200px] w-[148px] shadow-[0px_6px_8px_rgba(60,224,232,0.6)] hover:shadow-[0px_6px_8px_rgba(60,224,232,1.0)]"
+      className="flex flex-col pb-s rounded-lg bg-secondary transition-all duration-200 hover:-translate-y-1 h-[200px] w-[148px] shadow-[0px_6px_8px_rgba(60,224,232,0.6)] hover:shadow-[0px_6px_8px_rgba(60,224,232,1.0)]"
     >
-      <div className="relative h-[111px] w-full">
-        <Image className="object-cover" src={imageUrl} alt={imageAlt} fill />
+      <div className="relative h-[111px] w-full shrink-0 rounded-tl-lg rounded-tr-lg overflow-hidden">
+        <Image
+          className="object-contain"
+          src={imageUrl}
+          alt={imageAlt}
+          fill
+          sizes="148px"
+        />
       </div>
-      <div className={`flex flex-1 items-center px-s ${nameClassName}`}>{displayName}</div>
+
+      <div className="flex flex-1 items-center px-s">
+        {isTruncated ? (
+          <TooltipTrigger delay={300} closeDelay={300}>
+            <Focusable>
+              <div className={nameClassName}>
+                {displayName}
+              </div>
+            </Focusable>
+
+            <Tooltip className="max-w-[220px]">
+              {name}
+            </Tooltip>
+          </TooltipTrigger>
+        ) : (
+          <div className={nameClassName}>
+            {displayName}
+          </div>
+        )}
+      </div>
     </Link>
   );
 }
