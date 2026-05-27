44b2d01662383742a2c4d06416d1f563217ec50d
oniwawa <s243336@stn.nagaokaut.ac.jp>
2026-05-28 00:58:36 +0900

    [feat]pnpm fmt実行

--- src/components/ui/MapFrame.tsx
+++ src/components/ui/MapFrame.tsx
@@ -13,15 +13,13 @@ export default function MapFrame({ imageSrc, alt, title, type = "long" }: MapFra
       <div className="w-full px-10">
         <div className="flex w-full items-start items-stretch gap-0">
           <div
-            className={`bg-main px-4 text-text-large text-base py-[4px] ${type === "short" ? "w-[50px]" : "w-[194px]"}`}
+            className={`bg-main px-4 py-[4px] text-text-large text-base ${type === "short" ? "w-[50px]" : "w-[194px]"}`}
           >
             <span className="leading-tight break-words whitespace-normal">{title}</span>
           </div>
           <div className="-ml-px w-[20px] self-stretch bg-main [clip-path:polygon(0%_0%,20%_0%,100%_100%,0%_100%)]"></div>
         </div>
-        <div
-          className="aspect-4/3 relative flex items-center justify-center overflow-hidden border-2 border-main text-text-large"
-        >
+        <div className="relative flex aspect-4/3 items-center justify-center overflow-hidden border-2 border-main text-text-large">
           {imageSrc ? (
             <Image src={imageSrc} alt={alt || ""} fill className="object-cover" />
           ) : (
