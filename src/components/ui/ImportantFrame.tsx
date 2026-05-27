5b10fe62582ae5db4c85f33272214e5414c00ae6
timdaik <hirokix0608@gmail.com>
2026-05-27 21:16:22 +0900

    fix: #114 leadingの競合を削除 text-button に既に line-height のスタイルが 含まれているので

--- src/components/ui/ImportantFrame.tsx
+++ src/components/ui/ImportantFrame.tsx
@@ -9,9 +9,7 @@ export default function ImportantFrame({ title, children }: ImportantFrameProps)
   return (
     <div className="w-full border-y-[1.4px] border-main bg-base-dark py-l shadow-[0px_2px_6px_0px_#111D53] md:px-pl md:py-3l">
       <div className="mx-auto flex w-full max-w-260 flex-col gap-0.5 px-3l md:px-0">
-        <h3 className="font-sans text-button leading-7.25 text-accent md:text-Ptitle-small md:leading-8">
-          {title}
-        </h3>
+        <h3 className="font-sans text-button text-accent md:text-Ptitle-small">{title}</h3>
         <div className="font-sans text-textb text-white md:text-Ptext-large">{children}</div>
       </div>
     </div>
