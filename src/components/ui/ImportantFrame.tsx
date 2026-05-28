b075092f4786b6777f9856380c0627c6024f659f
timdaik <hirokix0608@gmail.com>
2026-05-28 21:47:12 +0900

    fix: ハードコードをカラーテーマ利用に修正

--- src/components/ui/ImportantFrame.tsx
+++ src/components/ui/ImportantFrame.tsx
@@ -7,7 +7,7 @@ type ImportantFrameProps = {
 
 export default function ImportantFrame({ title, children }: ImportantFrameProps) {
   return (
-    <div className="w-full border-y-[1.4px] border-main bg-base-dark py-l shadow-[0px_2px_6px_0px_#111D53] md:px-pl md:py-3l">
+    <div className="w-full border-y-[1.4px] border-main bg-base-dark py-l shadow-[0px_2px_6px_0px] shadow-shadow md:px-pl md:py-3l">
       <div className="mx-auto flex w-full max-w-260 flex-col gap-0.5 px-3l md:px-0">
         <h3 className="font-sans text-button text-accent md:text-Ptitle-small">{title}</h3>
         <div className="font-sans text-textb text-white md:text-Ptext-large">{children}</div>
