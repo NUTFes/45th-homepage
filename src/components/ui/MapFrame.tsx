91c43e0b4d19514e01ba54de6c987844075fe633
oniwawa <s243336@stn.nagaokaut.ac.jp>
2026-06-01 20:47:10 +0900

    [fix]修正

--- src/components/ui/MapFrame.tsx
+++ src/components/ui/MapFrame.tsx
@@ -15,7 +15,7 @@ export default function MapFrame({ imageSrc, alt, title, type = "long" }: MapFra
           <div
               className={`bg-main px-4 py-1 text-text-large text-base ${type === "short" ? "w-12.5" : "w-48.5"}`}
           >
-            <span className="leading-tight break-words whitespace-normal">{title}</span>
+             <span className="leading-tight wrap-break-word whitespace-normal">{title}</span>
           </div>
           <div className="-ml-px w-[20px] self-stretch bg-main [clip-path:polygon(0%_0%,20%_0%,100%_100%,0%_100%)]"></div>
         </div>
