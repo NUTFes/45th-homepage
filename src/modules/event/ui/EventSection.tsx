fce8a9d1f176c64ffad3f79dbecc7df48585a4bd
kiyo-jin <25.m.jinnguuji.nutfes@gmail.com>
2026-06-01 12:10:28 +0900

    feat: EventSectionコンポーネントにイベントプロパティを追加

--- src/modules/event/ui/EventSection.tsx
+++ src/modules/event/ui/EventSection.tsx
@@ -1,7 +1,21 @@
+import EventFrame, { type EventFrameProps } from "@/components/ui/EventFrame";
+import Link from "next/link";
 
+type EventSectionProps = {
+  title: string;
+  viewAllHref: string;
+  events: EventFrameProps[];
+};
 
-export default function EventSection() {
+export default function EventSection({ title, viewAllHref, events }: EventSectionProps) {
   return (
-    <p>あ</p>
+    <div className="gap-ss flex flex-col">
+      <div className="flex items-center justify-between pr-m pl-l">
+        <div className="font-kaisotai text-title text-font-main">{title}</div>
+        <Link href={viewAllHref} className="text-text text-font-main">
+          すべて表示
+        </Link>
+      </div>
+    </div>
   );
 }
