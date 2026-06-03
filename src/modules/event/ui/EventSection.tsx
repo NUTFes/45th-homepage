bfade77d8abc9da05fc1b438e2baae2d1fcb90a6
kiyo-jin <25.m.jinnguuji.nutfes@gmail.com>
2026-06-03 13:00:16 +0900

    style: EventSectionのリスト要素のクラス名を修正

--- src/modules/event/ui/EventSection.tsx
+++ src/modules/event/ui/EventSection.tsx
@@ -23,7 +23,7 @@ export default function EventSection({ title, viewAllHref, id, events }: EventSe
           すべて表示
         </Link>
       </div>
-      <ul role="list" className="flex gap-s overflow-x-auto pl-ll">
+      <ul role="list" className="flex gap-s overflow-x-auto px-ll [scrollbar-width:none]">
         {events.map((event) => (
           <li key={event.href} className="shrink-0">
             <EventFrame {...event} />
