820e5c867b2540a14f7d4fb1c90d81a0063f84af
kiyo-jin <25.m.jinnguuji.nutfes@gmail.com>
2026-06-03 09:26:30 +0900

    style: EventSectionのコードスタイルを改善

--- src/modules/event/ui/EventSection.tsx
+++ src/modules/event/ui/EventSection.tsx
@@ -4,20 +4,26 @@ import Link from "next/link";
 type EventSectionProps = {
   title: string;
   viewAllHref: string;
-  id:string;
+  id: string;
   events: EventFrameProps[];
 };
 
 export default function EventSection({ title, viewAllHref, id, events }: EventSectionProps) {
   return (
-    <section aria-labelledby={`section-${id}`} className="gap-ss flex flex-col">
+    <section aria-labelledby={`section-${id}`} className="flex flex-col gap-ss">
       <div className="flex items-center justify-between pr-m pl-l">
-        <div id={`section-${id}`} className="font-kaisotai text-title text-font-main">{title}</div>
-        <Link aria-label={`${title}を全て表示`} href={viewAllHref} className="text-text text-font-main">
+        <div id={`section-${id}`} className="font-kaisotai text-title text-font-main">
+          {title}
+        </div>
+        <Link
+          aria-label={`${title}をすべて表示`}
+          href={viewAllHref}
+          className="text-text text-font-main"
+        >
           すべて表示
         </Link>
       </div>
-      <ul role="list" className="pl-ll gap-s flex overflow-x-auto">
+      <ul role="list" className="flex gap-s overflow-x-auto pl-ll">
         {events.map((event) => (
           <li key={event.href} className="shrink-0">
             <EventFrame {...event} />
