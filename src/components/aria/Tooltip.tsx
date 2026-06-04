dfae81701be8a39b80cd75f54153dffe3a261768
TkymHrt <23.h.takayama.nutfes@gmail.com>
2026-06-04 21:39:27 +0900

    style: apply format

--- src/components/aria/Tooltip.tsx
+++ src/components/aria/Tooltip.tsx
@@ -38,7 +38,7 @@ export function Tooltip({ children, offset = 10, ...props }: TooltipProps) {
           width={8}
           height={8}
           viewBox="0 0 8 8"
-          className="group-placement-left:-rotate-90 group-placement-right:rotate-90 group-placement-bottom:rotate-180 block fill-white stroke-neutral-200 forced-colors:fill-[Canvas] forced-colors:stroke-[ButtonBorder]"
+          className="block fill-white stroke-neutral-200 group-placement-left:-rotate-90 group-placement-right:rotate-90 group-placement-bottom:rotate-180 forced-colors:fill-[Canvas] forced-colors:stroke-[ButtonBorder]"
         >
           <path d="M0 0 L4 4 L8 0" />
         </svg>
