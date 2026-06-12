1ba4d59ed953f576c6c2da67ea890b29f5ad0a74
kiyo-jin <25.m.jinnguuji.nutfes@gmail.com>
2026-06-13 00:31:13 +0900

    feat: aria-labelを追加

--- src/modules/event/ui/EventPageView.tsx
+++ src/modules/event/ui/EventPageView.tsx
@@ -4,7 +4,11 @@ import SectionTitle from "@/components/ui/SectionTitle";
 import { type EventFrameProps } from "@/components/ui/EventFrame";
 
 const DUMMY_EVENTS: EventFrameProps[] = [
-  { name: "イベント名サンプル1イベント名サンプルイベント名サンプル", href: "#1", imageUrl: "/favicon/45th-LogoBlue.svg" },
+  {
+    name: "イベント名サンプル1イベント名サンプルイベント名サンプル",
+    href: "#1",
+    imageUrl: "/favicon/45th-LogoBlue.svg",
+  },
   { name: "イベント名サンプル2", href: "#2", imageUrl: "/favicon/45th-LogoBlue.svg" },
   { name: "イベント名サンプル3", href: "#3", imageUrl: "/favicon/45th-LogoBlue.svg" },
   { name: "イベント名サンプル4", href: "#4", imageUrl: "/favicon/45th-LogoBlue.svg" },
@@ -19,24 +23,24 @@ const EVENT_SECTIONS = [
   { id: "section5", title: "企業ブース", viewAllHref: "#", events: DUMMY_EVENTS },
 ];
 
-export default function EventSectionPage() {
+export default function EventPageView() {
   return (
     <div className="flex flex-col gap-4l pb-4l">
-      <section className="flex flex-col gap-s">
+      <section aria-label="ゲスト" className="flex flex-col gap-s">
         <SectionTitle title="ゲスト" />
         <div className="flex justify-center">
           <ButtonMain href="/guest" title="もっと見る　＞" />
         </div>
       </section>
-      <section className="flex flex-col gap-3l">
+      <section aria-label="イベント・販売" className="flex flex-col gap-3l">
         <SectionTitle title="イベント・販売" />
         {EVENT_SECTIONS.map((section) => (
           <EventSection key={section.id} {...section} />
         ))}
       </section>
-      <section>
+      <section aria-label="協賛企業">
         <div className="flex justify-center">
-                  <ButtonMain href="/sponsors" title="ご協賛いただいた企業様　＞" />
+          <ButtonMain href="/sponsors" title="ご協賛いただいた企業様　＞" />
         </div>
       </section>
     </div>
