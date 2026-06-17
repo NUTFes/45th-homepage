414e266898c7858e75e7031510821c1ced8facb3
unsigned <naotoanzai50@gmail.com>
2026-06-18 01:31:31 +0900

    feat: ContactSection の大まかな構造の構成

--- src/components/ui/ContactSection.tsx
+++ src/components/ui/ContactSection.tsx
@@ -2,3 +2,18 @@ export type ContactSectionProps = {
   title?: string;
   body?: string;
 };
+
+export default function ContactSection({ title, body }: ContactSectionProps) {
+    return (
+        <div> {/*企画のタイトル・本文統合要素*/}
+            <div> {/*タイトル要素*/}
+                <div> {/*タイトル下のボーダー*/}
+                    <p>{title}</p>
+                </div>
+            </div>
+            <div> {/*本文要素*/}
+                <p>{body}</p>
+            </div>
+        </div>
+    )
+}
