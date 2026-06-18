2bcc04ab0884aa18d81556a8b1843cb01c6fa84e
unsigned <naotoanzai50@gmail.com>
2026-06-19 00:06:23 +0900

    feat: 4つ角以外のContactSection の作成

--- src/components/ui/ContactSection.tsx
+++ src/components/ui/ContactSection.tsx
@@ -5,15 +5,15 @@ export type ContactSectionProps = {
 
 export default function ContactSection({ title, body }: ContactSectionProps) {
     return (
-        <div> {/*企画のタイトル・本文統合要素*/}
-            <div> {/*タイトル要素*/}
-                <div> {/*タイトル下のボーダー*/}
-                    <p>{title}</p>
+            <div className="relative flex flex-col gap-s w-full bg-base px-m md:px-ll py-s md:py-m border-1 border-main overflow-hidden text-secondary"> {/*企画のタイトル・本文統合要素*/}
+                <div className="px-xs md:px-3l font-kaisotai"> {/*タイトル要素*/}
+                    <div className=" px-s md:px-3l border-b-1 border-secondary"> {/*タイトル下のボーダー*/}
+                        <p className="text-title-small md:text-title text-center">{title}</p>
+                    </div>
+                </div>
+                <div className="md:px-s"> {/*本文要素*/}
+                    <p className="font-sans text-text md:text-Ptext">{body}</p>
                 </div>
             </div>
-            <div> {/*本文要素*/}
-                <p>{body}</p>
-            </div>
-        </div>
-    )
+    );
 }
