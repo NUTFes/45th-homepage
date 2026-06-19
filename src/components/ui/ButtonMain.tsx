b1ba334f91ed01ac09638e2e1ce4a7eef3e013c9
oniwawa <s243336@stn.nagaokaut.ac.jp>
2026-06-19 21:47:27 +0900

    [fix]allowをarrowに変更

--- src/components/ui/ButtonMain.tsx
+++ src/components/ui/ButtonMain.tsx
@@ -1,24 +1,22 @@
 import Link from "next/link";
-import { ChevronRight } from 'lucide-react';
+import { ChevronRight } from "lucide-react";
 
 type ButtonMainProps = {
   href: string;
   title: string;
-  allow?:boolean;
+  arrow?: boolean;
 };
 
 export default function ButtonMain(props: ButtonMainProps) {
-  const { href, title, allow = true } = props;
+  const { href, title, arrow = true } = props;
 
   return (
     <Link
       href={href}
-      className="min-h-16.5 flex justify-center items-center gap-x-xs button-gradient rounded-full border-2 border-main px-l py-s text-button text-white shadow-[0px_6px_8px_rgba(60,224,232,0.6)] transition-all duration-200 hover:-translate-y-1 hover:shadow-[0px_6px_8px_rgba(60,224,232,1.0)] md:text-Pbutton"
+      className="button-gradient flex min-h-16.5 items-center justify-center gap-x-xs rounded-full border-2 border-main px-l py-s text-button text-white shadow-[0px_6px_8px_rgba(60,224,232,0.6)] transition-all duration-200 hover:-translate-y-1 hover:shadow-[0px_6px_8px_rgba(60,224,232,1.0)] md:text-Pbutton"
     >
       {title}
-      {allow && <ChevronRight className="size-5" />}
-        
-
+      {arrow && <ChevronRight className="size-5" />}
     </Link>
   );
 }
