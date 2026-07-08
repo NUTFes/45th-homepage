ab8358ef0e0b04ceab116e5e6837b2a9595bc86b
unsigned <naotoanzai50@gmail.com>
2026-07-08 20:07:50 +0900

    feat: コンポーネントの枠を作成

--- src/modules/event/programs/[slug]/ProgramDetailPageView.tsx
+++ src/modules/event/programs/[slug]/ProgramDetailPageView.tsx
@@ -3,3 +3,8 @@ import Image from "next/image";
 import EventInfoCard from "@/components/ui/EventInfoCard";
 import EventIntroFrame from "@/components/ui/EventIntroFrame";
 import SectionTitle from "@/components/ui/SectionTitle";
+
+export default function ProgramDetailPageView() {
+    return (
+    );
+}
