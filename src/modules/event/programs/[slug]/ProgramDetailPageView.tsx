5c164c25d22ca950a09f1caacec8b8ce89c6dbac
unsigned <naotoanzai50@gmail.com>
2026-07-08 20:03:43 +0900

    chore: 使うパーツをインポートする

---
+++ src/modules/event/programs/[slug]/ProgramDetailPageView.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+import EventInfoCard from "@/components/ui/EventInfoCard";
+import EventIntroFrame from "@/components/ui/EventIntroFrame";
+import SectionTitle from "@/components/ui/SectionTitle";
