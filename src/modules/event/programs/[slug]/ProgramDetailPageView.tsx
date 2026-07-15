ee52f61f51760d3e055bd23073dba3a0cb20ce42
unsigned <naotoanzai50@gmail.com>
2026-07-16 00:09:46 +0900

    feat: 各コンポーネントの大まかな構造を作成

--- src/modules/event/programs/[slug]/ProgramDetailPageView.tsx
+++ src/modules/event/programs/[slug]/ProgramDetailPageView.tsx
@@ -4,10 +4,27 @@ import EventInfoCard from "@/components/ui/EventInfoCard";
 import EventIntroFrame from "@/components/ui/EventIntroFrame";
 import SectionTitle from "@/components/ui/SectionTitle";
 
-export default function ProgramDetailPageView() {
+
+function EventCard() {
     return(
         <div>
-            
+            <h1></h1>
+            <div>
+                <Image />
+            </div>
+        </div>
+    )
+}
+
+export default function ProgramDetailPageView() {
+    return(
+        <div >
+            <div>
+                <SectionTitle title="企画" />
+                <EventCard />
+            </div>
+            <EventIntroFrame />
+            <EventInfoCard />
         </div>
     )   
 }
