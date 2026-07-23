049f511b99e9cfdd9c765e5dcb59bf955e55b179
TkymHrt <23.h.takayama.nutfes@gmail.com>
2026-07-24 03:05:49 +0900

    fix: タグを左詰めで配置

--- src/modules/event/ui/CategoryMenu.tsx
+++ src/modules/event/ui/CategoryMenu.tsx
@@ -44,7 +44,7 @@ export default function CategoryMenu({ className, onClose, variant, ...props }: 
       <CheckboxGroup
         {...props}
         aria-label={props["aria-label"] ?? `${categoryMenuLabels[variant]}のタグ検索`}
-        className="flex w-full flex-wrap items-center justify-between gap-x-s gap-y-l px-ll md:gap-x-3l md:gap-y-ll md:px-4l"
+        className="flex w-full flex-wrap items-center justify-start gap-x-s gap-y-l px-ll md:gap-x-3l md:gap-y-ll md:px-4l"
       >
         {items.map((item) => (
           <CategoryCard
