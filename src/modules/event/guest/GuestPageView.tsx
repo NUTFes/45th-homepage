96eee011000bfc19328f5191506e257bfdb5f490
timdaik <hirokix0608@gmail.com>
2026-07-28 15:24:10 +0900

    fix: PCサイドバー右端背景の高さを揃える

--- src/modules/event/guest/GuestPageView.tsx
+++ src/modules/event/guest/GuestPageView.tsx
@@ -188,8 +188,10 @@ export default function GuestPageView() {
           <EventIntroFrame {...INTRODUCTION} headingLevel={1} />
         </div>
 
-        <aside className="relative bg-base-dark xl:sticky xl:top-18 xl:z-20 xl:col-start-2 xl:row-start-1 xl:row-end-7 xl:h-[calc(100dvh-4.5rem)] xl:self-start xl:after:pointer-events-none xl:after:absolute xl:after:inset-y-0 xl:after:left-full xl:after:w-screen xl:after:bg-base-dark xl:after:content-['']">
-          <EventInfoCard {...GUEST_EVENT_INFO} />
+        <aside className="relative bg-base-dark xl:sticky xl:top-18 xl:z-20 xl:col-start-2 xl:row-start-1 xl:row-end-7 xl:h-[calc(100dvh-4.5rem)] xl:self-start">
+          <div className="relative min-h-full bg-base-dark xl:after:pointer-events-none xl:after:absolute xl:after:inset-y-0 xl:after:left-full xl:after:w-screen xl:after:bg-base-dark xl:after:content-['']">
+            <EventInfoCard {...GUEST_EVENT_INFO} />
+          </div>
         </aside>
 
         <GuestInformationSection
