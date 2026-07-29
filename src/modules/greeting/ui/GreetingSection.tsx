d07457f6e3a1b4cfc6374a5e105d04b517a6cdeb
unsigned <naotoanzai50@gmail.com>
2026-07-29 15:08:48 +0900

    feat: スマホ版のスタイルの作成

--- src/modules/greeting/ui/GreetingSection.tsx
+++ src/modules/greeting/ui/GreetingSection.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default type GreetingSectionProps = {
+export type GreetingSectionProps = {
   name: string;
   imageSrc: string;
   imageAlt: string;
@@ -8,17 +8,34 @@ export default type GreetingSectionProps = {
   greetingBody: string;
 };
 
-export default function GreetingSection({ name, imageSrc, imageAlt, greetingTitle, greetingBody }: GreetingSectionProps) {
+// 写真と名前をコンテナ幅に応じて常に等幅で並べるための共有クラス
+const itemWidth = "min-w-0 flex-1 basis-0";
+
+export default function GreetingSection({
+  name,
+  imageSrc,
+  imageAlt,
+  greetingTitle,
+  greetingBody,
+}: GreetingSectionProps) {
   return (
-    <div>
-        <div> {/* 写真 + 名前 */}
-            <Image src={imageSrc} alt={imageAlt} />
-            <div>{name}</div>
+    <div className="flex flex-col border-t border-b border-main bg-base-dark text-font-main">
+      <div className="flex flex-row gap-3l border-b border-main bg-base">
+        <Image
+          src={imageSrc}
+          alt={imageAlt}
+          width={96}
+          height={144}
+          className={`${itemWidth} aspect-[2/3] w-full border-2 border-main object-cover`}
+        />
+        <div className={`flex items-center justify-start ${itemWidth}`}>
+          <h1 className="text-title-small">{name}</h1>
         </div>
-        <div> {/* 挨拶文 */}
-            <h2>{greetingTitle}</h2>
-            <p>{greetingBody}</p>
-        </div>
+      </div>
+      <div className="px-ll py-ll">
+        <h1 className="text-textb">{greetingTitle}</h1>
+        <p className="text-text whitespace-pre-line">{"\n"+greetingBody}</p>
+      </div>
     </div>
   );
 }
