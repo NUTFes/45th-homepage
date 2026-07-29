f6b10b443e85a90b7f8013b9499fe16811f3c627
unsigned <naotoanzai50@gmail.com>
2026-07-29 12:43:54 +0900

    feat: Props の作成と、コンポーネントへの適応

--- src/modules/greeting/ui/GreetingSection.tsx
+++ src/modules/greeting/ui/GreetingSection.tsx
@@ -1,13 +1,23 @@
 import Image from "next/image";
 
-export default function GreetingSection() {
+export default type GreetingSectionProps = {
+  name: string;
+  imageSrc: string;
+  imageAlt: string;
+  greetingTitle: string;
+  greetingBody: string;
+};
+
+export default function GreetingSection({ name, imageSrc, imageAlt, greetingTitle, greetingBody }: GreetingSectionProps) {
   return (
     <div>
         <div> {/* 写真 + 名前 */}
-            <Image/>
-            <div></div>
+            <Image src={imageSrc} alt={imageAlt} />
+            <div>{name}</div>
         </div>
         <div> {/* 挨拶文 */}
+            <h2>{greetingTitle}</h2>
+            <p>{greetingBody}</p>
         </div>
     </div>
   );
