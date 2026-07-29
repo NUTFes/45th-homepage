39a5d5d90502c5bfb72341e9510cf0de3758ef7e
unsigned <naotoanzai50@gmail.com>
2026-07-29 19:20:35 +0900

    feat: PC版スタイルの作成

--- src/modules/greeting/ui/GreetingSection.tsx
+++ src/modules/greeting/ui/GreetingSection.tsx
@@ -21,25 +21,29 @@ export default function GreetingSection({
   greetingBody,
 }: GreetingSectionProps) {
   return (
-    <div className="flex flex-col border-t border-b border-main bg-base-dark text-font-main">
-      <div>
-        <div>
+    <div className="flex flex-col md:flex-row border-t border-b border-main md:pl-pm bg-base-dark text-font-main">
+      <div className="md:flex md:flex-1 md:flex-row md:items-center md:justify-center md:gap-ss">
+        <div className="md:[writing-mode:vertical-rl] md:text-title md:font-kaisotai">
             <h1>{nameEng}</h1>
         </div>
-        <div className="flex flex-row gap-3l border-b border-main bg-base">
-            <Image
-            src={imageSrc}
-            alt={imageAlt}
-            width={96}
-            height={144}
-            className={`${itemWidth} aspect-[2/3] w-full border-2 border-main object-cover`}
-            />
-            <div className={`flex items-center justify-start ${itemWidth}`}>
-            <h1 className="text-title-small">{name}</h1>
+        <div className="border-b border-main bg-base md:flex md:h-[909px] md:w-100 md:items-center md:justify-center md:py-4l">
+            <div className="flex flex-row items-center gap-3l md:w-full md:flex-col md:gap-m">
+                <Image
+                src={imageSrc}
+                alt={imageAlt}
+                width={96}
+                height={144}
+                className={`${itemWidth} aspect-[2/3] w-full border-2 border-main object-cover`}
+                />
+                <div
+                className={`flex items-center justify-start md:flex-col md:justify-center md:gap-m md:shrink-0 ${itemWidth}`}
+                >
+                <h1 className="text-title-small md:text-Ptitle-small">{name}</h1>
+                </div>
             </div>
         </div>
       </div>
-      <div className="px-ll py-ll">
+      <div className="px-ll md:pl-4l md:pr-pm py-ll">
         <h1 className="text-textb md:text-Ptitle-small">{greetingTitle}</h1>
         <p className="text-text md:text-Ptext whitespace-pre-line">{"\n"+greetingBody}</p>
       </div>
