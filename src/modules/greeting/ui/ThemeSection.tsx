1e6febb6a6f6cb393291696d0692e0269c22b324
oniwawa <s243336@stn.nagaokaut.ac.jp>
2026-08-07 00:17:23 +0900

    [fix]pc版のロゴの大きさを調整

--- src/modules/greeting/ui/ThemeSection.tsx
+++ src/modules/greeting/ui/ThemeSection.tsx
@@ -3,13 +3,20 @@ import Image from "next/image";
 export default function ThemeSection() {
   return (
     <div className="relative w-full overflow-hidden bg-base-dark text-white">
-      <div className="pointer-events-none absolute -top-4 right-0 z-0">
+      <div className="pointer-events-none absolute -top-4 md:-top-6 right-0 z-0">
         <Image
           src="/image/greeting/45th-logo-touka.svg"
           alt="45thロゴ"
           width={220}
           height={220}
-          className="opacity-90 mix-blend-screen"
+          className="opacity-90 mix-blend-screen md:hidden"
+        />
+                <Image
+          src="/image/greeting/45th-logo-touka.svg"
+          alt="45thロゴ"
+          width={300}
+          height={300}
+          className="opacity-90 mix-blend-screen md:flex hidden"
         />
       </div>
       <div className="z-10 flex flex-col justify-start gap-y-ss px-ll py-5l font-kaisotai text-title md:px-pl md:text-Ptitle">
