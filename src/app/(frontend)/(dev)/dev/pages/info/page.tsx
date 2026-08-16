f8ce80d6e93802ee6fecfb1fbffbc075c91a41ed
kiyo-jin <25.m.jinnguuji.nutfes@gmail.com>
2026-08-16 17:01:32 +0900

    feat: Info PageへのリンクをDevPageModulesIndexPageに追加

--- src/app/(frontend)/(dev)/dev/pages/info/page.tsx
+++ src/app/(frontend)/(dev)/dev/pages/info/page.tsx
@@ -1,7 +1,27 @@
+import InfoPageView from "@/modules/info/InfoPageView";
+
+import { DevPageContainer } from "../../_components/DevPageContainer";
+import { DevPanel } from "../../_components/DevPanel";
+import { DevSection } from "../../_components/DevSection";
+
+export const metadata = {
+  title: "Info Page Modules - Dev",
+  description: "src/modules/info のコンポーネントをページ文脈で確認",
+};
+
 export default function DevInfoPageModulesPage() {
   return (
-    <div>
-
-    </div>
+    <DevPageContainer
+      title="Info Page Modules"
+      description="src/modules/info のコンポーネントをページ文脈で確認"
+    >
+      <DevSection title="InfoPageView">
+        <DevPanel title="InfoPageView (src/modules/info)" fullWidth>
+          <div className="bg-base">
+            <InfoPageView />
+          </div>
+        </DevPanel>
+      </DevSection>
+    </DevPageContainer>
   );
 }
