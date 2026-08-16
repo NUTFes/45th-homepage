a00ed6cc28c3d75f99d0c00fcbd0601d943887b8
kiyo-jin <25.m.jinnguuji.nutfes@gmail.com>
2026-08-16 16:33:04 +0900

    feat: DevInfoPageModulesPageコンポーネントを新規作成

---
+++ src/app/(frontend)/(dev)/dev/pages/info/page.tsx
@@ -0,0 +1,7 @@
+export default function DevInfoPageModulesPage() {
+  return (
+    <div>
+
+    </div>
+  );
+}
--- src/modules/info/InfoPageView.tsx
+++ src/modules/info/InfoPageView.tsx
@@ -1,1 +1,9 @@
+import ImportantFrame from "@/components/ui/ImportantFrame";
 
+export default function InafoPageView() {
+  return (
+    <div>
+
+    </div>
+  );
+}
