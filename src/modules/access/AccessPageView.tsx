d8a52992c46e6e4500acc004ef433cb757c444eb
oniwawa <s243336@stn.nagaokaut.ac.jp>
2026-08-26 22:19:51 +0900

    [feat]トラパリンクを追加

--- src/modules/access/AccessPageView.tsx
+++ src/modules/access/AccessPageView.tsx
@@ -2,15 +2,18 @@ import SectionTitle from "@/components/ui/SectionTitle";
 import InfoFrame from "@/components/ui/InfoFrame";
 import Image from "next/image";
 import MapFrame from "@/components/ui/MapFrame";
+import ButtonMain from "@/components/ui/ButtonMain";
 //import AdCarousel from "@/components/ui/AdCarousel"; ←まだない
 
 function AccessPageMobile() {
   return (
     <div className="flex min-h-screen flex-col gap-y-4l bg-base py-pm text-white">
-      <div className="gap-y-s">
-        <SectionTitle title="アクセス" />
-        <div className="w-full text-center text-button">大学Googleマップ</div>
-        <div className="flex justify-center px-8 py-s">
+     
+      <div className="flex flex-col gap-y-ss">
+        <SectionTitle title="アクセス" /> 
+        <div className="flex flex-col gap-y-1">
+        <div className="w-full text-center text-button">大学Googleマップ</div>     
+        <div className="flex justify-center px-8">
           <iframe
             className="aspect-4/3 w-full max-w-4xl border-2 border-main"
             src="https://www.google.com/maps?q=%E9%95%B7%E5%B2%A1%E6%8A%80%E8%A1%93%E7%A7%91%E5%AD%A6%E5%A4%A7%E5%AD%A6%20%E6%96%B0%E6%BD%9F%E7%9C%8C%E9%95%B7%E5%B2%A1%E5%B8%82%E4%B8%8A%E5%AF%8C%E5%B2%A1%E7%94%BA1603-1&output=embed"
@@ -20,19 +23,24 @@ function AccessPageMobile() {
           />
         </div>
         <div className="w-full text-center text-text">「技大前」バス停からすぐ</div>
+        </div>
       </div>
       <InfoFrame className="flex w-full flex-col gap-y-s px-8 text-center">
-        <div className="pb-s">
-          <SectionTitle title="開催日" pb/>
+        <div className="flex flex-col gap-y-s">
+          <div className="flex justify-center pb-s">
+            <SectionTitle title="開催日" pb />
+          </div>
+          <div className="text-button">9月19日（土）　10：00～20：00 </div>
+          <div className="text-button">9月20日（日）　10：00～18：30 </div>
+          <div className="text-text text-start">
+            ※1日目の出店は17：00まで、2日目の出店は16：00までです
+          </div>
         </div>
-        <div className="text-button">9月19日（土）　10：00～20：00 </div>
-        <div className="text-button">9月20日（日）　10：00～18：30 </div>
-        <div className="text-text">※1日目の出店は17：00まで、2日目の出店は16：00までです</div>
       </InfoFrame>
 
       <div className="flex flex-col gap-y-m">
         <SectionTitle title="シャトルバス時刻表" />
-        <div className="px-3l text-text">
+        <div className="px-m text-text">
           <p>技大祭当日は、専用のシャトルバスを運行します。</p>
           <p>
             通常の路線バスとは<span className="text-accent">乗り場が異なります</span>
@@ -42,17 +50,18 @@ function AccessPageMobile() {
             乗り場周辺には、技大祭Tシャツを着たスタッフがおりますので、場所がわからない場合はお気軽にお声がけください。
           </p>
         </div>
-        <div className="justify-center gap-y-ss text-center">
+        <div className="justify-center gap-y-ss px-l text-center">
           <Image
             src="/image/access/shuttle_pc.png"
             alt="シャトルバスの時刻表"
             width={754}
             height={598}
-            sizes="100vw"
-            className="mx-auto h-auto w-full max-w-83.5 px-3l md:hidden"
+            className="mx-auto h-auto w-full max-w-none"
           />
         </div>
-        <div className="flex flex-col gap-y-m bg-base-dark py-l">
+      
+      </div>
+  <div className="flex flex-col gap-y-m bg-base-dark py-l">
           <div className="flex w-full flex-col gap-y-xs px-ll">
             <div className="underline-secondary text-title-small underline">シャトルバス実施日</div>
             <div className="text-Ptext-large">2026年9月19日（土）・20日（日）</div>
@@ -98,25 +107,25 @@ function AccessPageMobile() {
             />
           </div>
         </div>
-      </div>
-
       <div className="flex flex-col gap-y-m">
         <SectionTitle title="路線バス時刻表" />
-        <div className="flex flex-col px-ll text-Ptext">
+        <div className="flex flex-col px-ll text-text">
           <span>長岡駅-技大前間の通常の路線バスです。</span>
           <span>乗り場は通常の路線バス乗り場となります。</span>
           <span className="text-accent">乗車には運賃430円が必要です。</span>
 
           <span>お困りの際はお近くの実行委員にお尋ねください。</span>
         </div>
-        <Image
-          src="/image/access/rosen_pc.png"
-          alt="路線バスの時刻表"
-          width={754}
-          height={908}
-          sizes="100vw"
-          className="mx-auto h-auto w-full max-w-83.5 px-3l md:hidden"
-        />
+        <div className="px-l md:hidden">
+          <Image
+            src="/image/access/rosen_pc.png"
+            alt="路線バスの時刻表"
+            width={754}
+            height={908}
+            sizes="100vw"
+            className="mx-auto h-auto w-full max-w-none"
+          />
+        </div>
 
         <div className="flex flex-col justify-center gap-y-ss px-ll text-center text-text">
           <div className="text-text-small">
@@ -128,8 +137,16 @@ function AccessPageMobile() {
       <div className="flex flex-col gap-y-m">
         <SectionTitle title="駐車場について" />
         <div className="flex flex-col justify-center gap-y-ss px-ll text-center text-text">
+         <div className="text-button">駐車場マップ</div>
           <MapFrame />
-          <div className="text-text">
+          <div className="w-full flex flex-col items-center py-m">
+            <div className="w-80">
+            <ButtonMain title="駐車場の空き状況はこちら" href="https://t.nutfes.net/?id=r9vi6gg&p=web" newTab arrow={true}/>
+              </div>
+
+          </div>
+          
+          <div className="text-text text-start">
             <b>駐車場には限りがございます。</b>
             当日は大変な混雑が予想されますので、できるだけ公共交通機関をご利用くださいますようお願いいたします。
             また、近隣住民の皆様のご迷惑となりますので、
@@ -180,13 +197,15 @@ function AccessPageDesktop() {
                 className="pointer-events-none absolute -top-pl left-[calc((100vw-100%)/-2)] z-0 hidden md:block"
               />
               <InfoFrame className="relative z-10 flex flex-col gap-y-s px-4l text-center">
-                <div className="pb-s">
-                  <SectionTitle title="開催日" pb/>
-                </div>
-                <div className="text-button">9月19日（土）　10：00～20：00 </div>
-                <div className="text-button">9月20日（日）　10：00～18：30 </div>
-                <div className="text-text">
-                  ※1日目の出店は17：00まで、2日目の出店は16：00までです
+                <div className="flex flex-col gap-y-s">
+                  <div className="flex justify-center pb-s">
+                    <SectionTitle title="開催日" pb />
+                  </div>
+                  <div className="text-button">9月19日（土）　10：00～20：00 </div>
+                  <div className="text-button">9月20日（日）　10：00～18：30 </div>
+                  <div className="text-text">
+                    ※1日目の出店は17：00まで、2日目の出店は16：00までです
+                  </div>
                 </div>
               </InfoFrame>
             </div>
@@ -312,6 +331,16 @@ function AccessPageDesktop() {
             <div className="relative mx-auto w-full max-w-200">
               <div className="text-Ptitle-small">駐車場マップ</div>
               <MapFrame />
+              <div className="flex w-full flex-col items-center py-m">
+                <div className="w-100">
+                  <ButtonMain
+                    title="駐車場の空き状況はこちら"
+                    href="https://t.nutfes.net/?id=r9vi6gg&p=web"
+                    newTab
+                    arrow={true}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -326,7 +355,9 @@ export default function AccessPageView() {
       <div className="md:hidden">
         <AccessPageMobile />
       </div>
-      <AccessPageDesktop />
+      <div className="hidden md:block">
+        <AccessPageDesktop />
+      </div>
     </>
   );
 }
