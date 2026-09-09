2840d4cc248300a83ad24e77da61db63d02a1b11
naoto-anzai <119020943+naoto-anzai@users.noreply.github.com>
2026-09-10 03:20:29 +0900

    feat: 現時刻が、どの区間に属するのかを求める関数

--- src/modules/top/popup/popupSchedule.ts
+++ src/modules/top/popup/popupSchedule.ts
@@ -3,3 +3,15 @@ export const POPUP_WINODWS = [
     {start: 11, end: 16}, // 1回目
     {start: 16, end: 27}, // 27 時は、翌３時
 ]
+
+// 今が、上記で定義した区間のどれに属するか、その区間の開始時間を計算する
+const nowWindowStart = (now: Date): number | null => {
+    const nowHours = now.getHours();
+    for(let pWindow of POPUP_WINODWS){
+        if(nowHours >= pWindow.start && nowHours < pWindow.end){
+            return pWindow.start; 
+        }
+    }
+
+    return null;
+}
